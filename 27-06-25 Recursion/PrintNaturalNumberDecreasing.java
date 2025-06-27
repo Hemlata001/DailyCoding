148b8a5cd3d2636eea595836de1fe9f79f890c66
Hemlata <127768311+Hemlata001@users.noreply.github.com>
2025-06-27 23:35:55 +0530

    PrintNaturalNumberDecreasing.java

--- 27-06-25 Recursion/PrintNaturalNumberDecreasing.java
+++ 27-06-25 Recursion/PrintNaturalNumberDecreasing.java
@@ -7,7 +7,7 @@ public class Main
             return;
         }
         System.out.println(n);
-        printNaturalNumber(n-1);
+        printNaturalNumberDecreasing(n-1);
     }
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
