b0d118433f934a34165fdb2d1311a95b8a38f12e
Hemlata <127768311+Hemlata001@users.noreply.github.com>
2025-07-02 11:56:30 +0530

    LCM.java

---
+++ 01-07-2025 Recursion/LCM.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main
+{
+    public static int gcd(int x, int y){
+        if(y==0){
+            return x;
+        }
+        return gcd(y,x%y);
+    }
+    public static int lcm(int x, int y) {
+        return (x * y) / gcd(x, y);
+    }
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+  		System.out.print("Enter the value of x: ");
+  		int x = sc.nextInt();
+  		System.out.print("Enter the value of y: ");
+  		int y = sc.nextInt();
+  		System.out.println("GCD of " + x + " and " + y +" is : " +  gcd(x,y));
+  		System.out.println("LCM of " + x + " and " + y + " is: " + lcm(x, y));
+		
+	}
+}
