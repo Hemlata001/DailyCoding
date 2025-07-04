23bf7fc26a76dfadc9deb8c98c09e60c8db94f7d
Hemlata <127768311+Hemlata001@users.noreply.github.com>
2025-07-04 19:21:08 +0530

    SearchInRotatedSortedArray.java

---
+++ 03-07-2025 Divide&Conquer/SearchInRotatedSortedArray.java
@@ -0,0 +1,14 @@
+/*leetcode
+  33. Search in Rotated Sorted Array*/
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        for(int i = 0;i<n;i++){
+            if(nums[i]== target){
+                return i;
+            }
+        }
+        return -1;
+        
+    }
+}
