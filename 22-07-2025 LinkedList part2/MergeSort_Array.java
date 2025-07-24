8c3716ba2ad963013792ed67643bd0cdac18fd14
Hemlata <127768311+Hemlata001@users.noreply.github.com>
2025-07-24 12:29:02 +0530

    Create MergeSort_Array.java

---
+++ 22-07-2025 LinkedList part2/MergeSort_Array.java
@@ -0,0 +1,49 @@
+# Geeks for Geeks
+  class Solution {
+     void mergeSort(int[] arr, int l, int r) {
+        if (l >= r) {
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid + 1, r);
+
+        merge(arr, l, mid, r);
+    }
+
+    public static void merge(int[] arr, int l, int mid, int r) {
+        int n1 = mid - l + 1;
+        int n2 = r - mid;
+
+        int[] leftArr = new int[n1];
+        int[] rightArr = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            leftArr[i] = arr[l + i];
+        }
+
+        for (int j = 0; j < n2; j++) {
+            rightArr[j] = arr[mid + 1 + j];
+        }
+
+        int i = 0, j = 0, k = l;
+
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k++] = leftArr[i++];
+            } else {
+                arr[k++] = rightArr[j++];
+            }
+        }
+
+        while (i < n1) {
+            arr[k++] = leftArr[i++];
+        }
+
+        while (j < n2) {
+            arr[k++] = rightArr[j++];
+        }
+    }
+}
