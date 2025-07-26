a723f6fd1d8b73ad5f993f89170e5400c64e8382
Hemlata <127768311+Hemlata001@users.noreply.github.com>
2025-07-26 17:39:03 +0530

    Create ValidParentheses.java

---
+++ 25-07-2025 Stack/ValidParentheses.java
@@ -0,0 +1,28 @@
+// leetcode  20. Valid Parentheses
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character>st = new Stack<>();
+        int n = s.length();
+        for(int i = 0;i<n;i++){
+            char ch = s.charAt(i);
+
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            }
+            else{
+                if(st.isEmpty()) return false;
+                char top = st.peek();
+                if((ch == ')' && top == '(')||
+                (ch =='}' && top =='{') ||
+                (ch ==']' && top == '[')){
+                    st.pop();
+                }else{
+                    return false;
+                }
+            }
+
+        }
+        return st.isEmpty();
+        
+    }
+}
