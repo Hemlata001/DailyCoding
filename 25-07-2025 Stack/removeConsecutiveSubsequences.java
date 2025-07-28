1796b6435878802d3aa2cad129badc2af3e8969e
Hemlata <127768311+Hemlata001@users.noreply.github.com>
2025-07-28 20:35:18 +0530

    removeConsecutiveSubsequences.java

---
+++ 25-07-2025 Stack/removeConsecutiveSubsequences.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class removeConsecutiveSubsequences
+{
+    public static int[] remove(int[] arr){
+        int n = arr.length;
+        Stack<Integer>st = new Stack<>();
+        for(int i = 0;i<n;i++){
+            if(st.isEmpty() || st.peek()!= arr[i]){
+                st.push(arr[i]);
+            }
+            else if(arr[i] == st.peek()){
+                if(i == n-1 || arr[i]!=arr[i+1]){
+                    st.pop();
+                }
+            }
+        }
+        int[] res = new int[st.size()];
+        int m = res.length;
+        for(int i = m-1;i>=0;i--){
+            res[i] = st.pop();
+        }
+        return res;
+    }
+    public static void printArray(int[] arr) {
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+	public static void main(String[] args) {
+        int[] arr = {1,2,2,3,10,10,10,4,4,4,5,7,7,2};
+        System.out.println("Actual Array");
+        printArray(arr);
+
+        int[] res = remove(arr);
+
+        System.out.println("Resultant Array");
+        printArray(res);
+    }
+}
