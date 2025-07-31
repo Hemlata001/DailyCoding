871b2b601fe93b8284a784d6376d9c422a8def24
Hemlata <127768311+Hemlata001@users.noreply.github.com>
2025-07-31 20:09:33 +0530

    Create Basics.java

---
+++ 31-07-2025 Queue/Basics.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Queue<Integer>q = new LinkedList<>();
+		q.add(1);
+		q.add(2);
+		q.add(3);
+		q.add(4);
+		System.out.println(q);
+	}
+}
