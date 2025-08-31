2653b05922950aee7a1d2697eb382ab70eaf2314
Hemlata <127768311+Hemlata001@users.noreply.github.com>
2025-08-31 17:59:39 +0530

    Create peekHeap.java

---
+++ 30-08-2025 PriorityQueue And Heap/peekHeap.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main
+{
+    static class Heap{
+        ArrayList<Integer> arr = new ArrayList<>();
+        
+        public void add(int data){
+            // add
+            arr.add(data);
+            int x = arr.size()-1;
+            int par = (x-1)/2;
+            
+            while(arr.get(x) < arr.get(par)){
+                // swap
+                int temp = arr.get(x);
+                arr.set(x,arr.get(par));
+                arr.set(par,temp);
+            }
+        }
+        
+        public int peek(){
+            return arr.get(0);
+        }
+    }
+}
